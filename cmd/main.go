7ea01bf3eb0c338a41c9f4d9e39c02f6fe8b8f95
Sergey Baraulia <baraulia@yandex.ru>
2022-02-06 19:55:55 +0300

    Added CI/CD

--- cmd/main.go
+++ cmd/main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", Hello)
-	err := http.ListenAndServe(":9000", nil)
+	err := http.ListenAndServe("159.223.1.135:80", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
