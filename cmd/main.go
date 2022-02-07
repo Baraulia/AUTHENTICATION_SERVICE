cee0f0031a6292d477e4af78b63893bdc57a581b
Anton <tadmopar@gmail.com>
2022-02-07 22:26:52 +0300

    fix

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"github.com/Baraulia/AUTHENTICATION_SERVICE/database"
 	"github.com/Baraulia/AUTHENTICATION_SERVICE/router"
 	"github.com/spf13/viper"
 	"log"
-	"net/http"
 )
 
 func main() {
@@ -23,11 +21,5 @@ func main() {
 	router := router.NewRoutes()
 	port := viper.GetString("PORT")
 	log.Fatal(router.Run(":" + port))
-
-<<<<<<< HEAD
-=======
-func Hello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello team! Auth service stage")
->>>>>>> 02fca46779994e8dc2241cbb0bb4dc18295bdf59
 }
 
